ca62db5a7d6d8ea453baa224c082e20cd2559088
Josh Wasserman <github email address>
2018-01-28 11:35:24 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import './App.css';
-import { connect } from 'react-redux';
-import { addItem } from  './actions/items';
+import React, { Component } from "react";
+import "./App.css";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { addItem } from "./actions/items";
 
 class App extends Component {
-
   handleOnClick() {
     this.props.store.dispatch(addItem());
   }
@@ -12,19 +12,26 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <button onClick={(event) => this.handleOnClick(event)}>
-          Click
-          </button>
+        <button onClick={event => this.handleOnClick(event)}>Click</button>
         <p>{this.props.items.length}</p>
       </div>
     );
   }
-};
+}
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     items: state.items
   };
 };
 
-export default connect(mapStateToProps)(App);
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators(
+    {
+      addItem: addItem
+    },
+    dispatch
+  );
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
